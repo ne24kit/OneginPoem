7dd6620af8f0453d45914bb261e46efef3b94c0c
nikita <ne24kit@gmail.com>
2023-09-25 11:29:02 +0300

    Replaced left_skip_punctuation and right_skip_punctuation with the skip_punctuation function

--- text.cpp
+++ text.cpp
@@ -1,34 +1,22 @@
 #include "text.h"
 
-#define cat(a, b) a##b
-
-void write_sorted(FILE * fp, Text *text);
-void write_source(FILE * fp, Text *text);
-
-int main(int argc, char *argv[])
+FILE *init_read_file(int argc, char *argv[])
 {
-    Text text;
-    FILE * fp_read = init_file(argc, argv);
-    FILE * fp_write;
-    fp_write = fopen("output.txt", "w+");
+    FILE *fp = NULL;
 
-    init_Text_from_file(&text, fp_read);
-    close_file(fp_read);
-    //qsort(text.poem, text.num_lines, sizeof(char *), comp);
+    MY_ASSERT((argc == 2), "Write ./a.out <filename.txt>\n");
+    MY_ASSERT(((fp = fopen(argv[1], "r")) != NULL), "Сan't open file "
+                                                    "for reading\n");
 
-    sort(text.poem, 0, text.num_lines - 1, sizeof(char **), comp);
-    write_sorted(fp_write, &text);
-    
-    sort(text.poem, 0, text.num_lines - 1, sizeof(char **), right_comp);
-    write_sorted(fp_write, &text);
-    
-    write_source(fp_write, &text);
-    
-    fclose(fp_write);
-    
-    print_text(text.poem, text.num_lines);
-    
-    clear_struct(&text);
+    return fp;
+}
+
+FILE *init_write_file(const char * filename)
+{
+    FILE *fp = NULL;
+    MY_ASSERT(((fp = fopen(filename, "w+")) != NULL), "Сan't open file "
+                                                      "for writing\n");
+    return fp;
 }
 
 void write_sorted(FILE * fp, Text *text)
@@ -50,8 +38,12 @@ void write_source(FILE * fp, Text *text)
     
 }
 
+void close_file(FILE * fp)
+{
+    MY_ASSERT((fp != NULL), "Pointer to file == NULL\n");
+    MY_ASSERT((fclose(fp) == 0), "File close error\n");
+}
 
-//TODO: change void to type of error and error handling
 void init_Text_from_file(Text *text, FILE *fp)
 {
     MY_ASSERT((fp != NULL), "Pointer to file == NULL\n");
@@ -60,7 +52,7 @@ void init_Text_from_file(Text *text, FILE *fp)
     text->size_buffer = get_size_file(fp);
     text->buffer = (char *)calloc(text->size_buffer + 1 + 1, sizeof(char));
     
-    //ставлю '\0', для обратной сортировки
+    //ставлю '\0' в начало для обратной сортировки
     *text->buffer = '\0';
     //перемещаю указатель
     text->buffer++;
@@ -72,39 +64,6 @@ void init_Text_from_file(Text *text, FILE *fp)
     text->poem = (char **)calloc(text->num_lines, sizeof(char *));
     set_lines_to_text(text);
 
-}
-
-void clear_struct(Text *text)
-{
-    //перемещаю указатель на '\0', чтобы не забыть отчистить его
-    text->buffer--;
-
-    free(text->buffer);
-    free(text->poem);
-
-    text->buffer = NULL;
-    text->poem = NULL;
-    text->size_buffer = -1;
-    text->num_lines = -1;
-    text = NULL;
-}
-
-
-//TODO: divide to two functions 
-FILE *init_file(int argc, char *argv[])
-{
-    FILE *fp = NULL;
-
-    MY_ASSERT((argc == 2), "Write ./a.out <filename.txt>\n");
-    MY_ASSERT(((fp = fopen(argv[1], "r")) != NULL), "Сan't open file\n");
-
-    return fp;
-}
-
-void close_file(FILE * fp)
-{
-    MY_ASSERT((fp != NULL), "Pointer to file == NULL\n");
-    MY_ASSERT((fclose(fp) == 0), "File close error\n");
 }
 
 void read_to_buffer(Text *text, FILE *fp)
@@ -144,7 +103,7 @@ size_t get_size_file(FILE * fp)
     
     return total_byte;
 }
-//rename: and delete '\n'
+
 void set_num_lines(Text *text)
 {
     MY_ASSERT((text != NULL), "Pointer to struct text == NULL\n");
@@ -158,8 +117,6 @@ void set_num_lines(Text *text)
             text->num_lines++;
         }
     }
-    //TODO: maybe without count, use only text->num_lines and set 0 at start???
-    
 }
 
 void print_text(char **text, size_t num_lines)
@@ -169,19 +126,25 @@ void print_text(char **text, size_t num_lines)
         printf("%s\n", text[i]);
 }
 
-const char * left_skip_punctuation(const char *str)
+void clear_Text_struct(Text *text)
 {
-    while(*str && !isalnum(*str))
-        str++;
+    //перемещаю указатель на '\0', чтобы не забыть отчистить его
+    text->buffer--;
 
-    return str;
+    free(text->buffer);
+    free(text->poem);
+
+    text->buffer = NULL;
+    text->poem = NULL;
+    text->size_buffer = -1;
+    text->num_lines = -1;
+    text = NULL;
 }
 
-const char * right_skip_punctuation(const char *str)
+const char * skip_punctuation(const char *str, int shift)
 {
-    
     while(*str && !isalnum(*str))
-        str--;
+        str += shift;
 
     return str;
 }
@@ -190,12 +153,15 @@ int right_comp(const void *a, const void *b)
 {   
     const char * str1 = *(const char **)a;
     const char * str2 = *(const char **)b;
+    
     str1 += strlen(str1) - 1;
     str2 += strlen(str2) - 1;
+
+    signed char shift = -1;
     
     while(*str1 && *str2){
-        str1 = right_skip_punctuation(str1);
-        str2 = right_skip_punctuation(str2);
+        str1 = skip_punctuation(str1, shift);
+        str2 = skip_punctuation(str2, shift);
 
         if(*str1 && *str2 && (toupper(*str1) != toupper(*str2)))
             return toupper(*str1) - toupper(*str2);
@@ -208,15 +174,16 @@ int right_comp(const void *a, const void *b)
     return *str1 - *str2;
 }
 
-int comp(const void *a, const void *b)
+int left_comp(const void *a, const void *b)
 {   
     const char * str1 = *(const char **)a;
     const char * str2 = *(const char **)b;
 
+    signed char shift = 1;
 
     while(*str1 && *str2){
-        str1 = left_skip_punctuation(str1);
-        str2 = left_skip_punctuation(str2);
+        str1 = skip_punctuation(str1, shift);
+        str2 = skip_punctuation(str2, shift);
 
         if(*str1 && *str2 && (toupper(*str1) != toupper(*str2)))
             return toupper(*str1) - toupper(*str2);
--- text.h
+++ text.h
@@ -13,10 +13,16 @@ typedef struct {
     size_t size_buffer = 0;
     char **poem = NULL;
     size_t num_lines = 0;
-} Text;
+}Text;
 
-FILE *init_file(int argc, char *argv[]);
+FILE *init_read_file(int argc, char *argv[]);
+FILE *init_write_file(const char * filename);
 void  close_file(FILE * fp);
+
+void write_sorted(FILE * fp, Text *text);
+void write_source(FILE * fp, Text *text);
+
+void init_Text_from_file(Text *text, FILE *fp);
 
 void read_to_buffer(Text *text, FILE *fp);
 void set_lines_to_text(Text *text);
@@ -26,11 +32,8 @@ void set_num_lines(Text *text);
 
 void print_text(char **text, size_t num_lines);
 
-void init_Text_from_file(Text *text, FILE *fp);
-void clear_struct(Text *text);
+void clear_Text_struct(Text *text);
 
-int comp(const void *a, const void *b);
-const char * left_delete_punctuation(const char *str);
-
+const char * skip_punctuation(const char *str, int shift);
+int left_comp(const void *a, const void *b);
 int right_comp(const void *a, const void *b);
-const char * right_skip_punctuation(const char *str);
